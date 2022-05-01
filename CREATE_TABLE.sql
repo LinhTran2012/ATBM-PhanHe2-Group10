6150ff106d39c4026fc460ed9bbe6ff37687b795
LinhTran2012 <tranlinh981@gmail.com>
2022-05-01 15:42:28 +0700

    Update

--- CREATE_TABLE.sql
+++ CREATE_TABLE.sql
@@ -82,7 +82,7 @@ CREATE TABLE NHANVIEN(
 	QUEQUAN VARCHAR2(50),
 	SODT CHAR(10),
 	CSYT VARCHAR2(10),
-	VAITRO VARCHAR2(20) CHECK (VAITRO IN (N'Thanh tra', N'Co so y te', N'Y sy/bac sy', N'Nghien cuu')),
+	VAITRO VARCHAR2(20) CHECK (VAITRO IN (N'Thanh tra', N'Co so y te', N'Y si/bac si', N'Nghien cuu')),
 	CHUYENKHOA VARCHAR2(10),
 	CONSTRAINT NHANVIEN_PK PRIMARY KEY ( MANV )
 );
@@ -136,10 +136,24 @@ AS
 BEGIN
     INSERT INTO NHANVIEN ("MANV", "HOTEN", "PHAI","NGAYSINH","CMND","QUEQUAN", "SODT","CSYT", "VAITRO", "CHUYENKHOA") 
     VALUES (manv, hoten, phai,TO_DATE(ngaysinh,'yyyy-mm-dd'),cmnd ,quequan, sodt, csyt, vaitro, chuyenkhoa);
-    strSQL :='CREATE USER "'||manv||'" IDENTIFIED BY "'||cmnd||'"';
+    strSQL :='CREATE USER "'||manv||'" IDENTIFIED BY "'||manv||'"';
     EXECUTE IMMEDIATE (strSQL);
     strSQL :='GRANT R_NHANVIEN TO "'||manv||'"';
     EXECUTE IMMEDIATE (strSQL);
+    
+    IF (vaitro = 'Thanh tra' ) THEN
+        strSQL :='GRANT R_THANHTRA TO "'||manv||'"';
+        EXECUTE IMMEDIATE (strSQL);
+    ELSIF (vaitro ='Co so y te') THEN
+        strSQL :='GRANT R_COSOYTE TO "'||manv||'"';
+        EXECUTE IMMEDIATE (strSQL);
+    ELSIF (vaitro ='Y si/bac si') THEN
+        strSQL :='GRANT R_YSI_BACSI TO "'||manv||'"';
+        EXECUTE IMMEDIATE (strSQL);
+    ELSIF (vaitro ='Nghien cuu') THEN
+        strSQL :='GRANT R_NGHIENCUU TO "'||manv||'"';
+        EXECUTE IMMEDIATE (strSQL);
+    END IF;
 END;
 
 CREATE OR REPLACE PROCEDURE sp_THEMBENHNHAN(
@@ -160,7 +174,7 @@ AS
 BEGIN
     INSERT INTO BENHNHAN ("MABN","MACSYT","TENBN","CMND","NGAYSINH","SONHA","TENDUONG","QUANHUYEN","TINHTP","TIENSUBENH","TIENSUBENHGD","DIUNGTHUOC")
     VALUES (mabn,macsyt,tenbn,cmnd,TO_DATE(ngaysinh,'yyyy-mm-dd'),sonha,tenduong,quanhuyen,tinhtp,tiensubenh,tiensubenhgd,diungthuoc);
-    strSQL :='CREATE USER "'||mabn||'" IDENTIFIED BY "'||cmnd||'"';
+    strSQL :='CREATE USER "'||mabn||'" IDENTIFIED BY "'||mabn||'"';
     EXECUTE IMMEDIATE (strSQL);
     strSQL :='GRANT R_BENHNHAN TO "'||mabn||'"';
     EXECUTE IMMEDIATE (strSQL);
--- INSERT_DATA.sql
+++ INSERT_DATA.sql
@@ -2,7 +2,7 @@
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS1','Benh vien da khoa Hong Duc','32/2 Thong Nhat - Phuong 10 - Quan Go Vap - TPHCM ', '0283996999');
 
 EXEC sp_THEMNHANVIEN('NV1',N'Do Khanh Ly', N'Nu', '2001-12-12','371953100', N'Kien Giang', '0868706271', 'CS1', N'Thanh tra', NULL);
-EXEC sp_THEMNHANVIEN('NV2',N'Do Tuong Vy', N'Nu', '2000-12-12','371953101', N'Kien Giang', '0777009958', 'CS1', N'Y sy/bac sy', 'CK1');
+EXEC sp_THEMNHANVIEN('NV2',N'Do Tuong Vy', N'Nu', '2000-12-12','371953101', N'Kien Giang', '0777009958', 'CS1', N'Y si/bac si', 'CK1');
 
 EXEC sp_THEMBENHNHAN('BN1','CS1', N'Tran Van Anh','916773394', '1954-03-11', 148, N'3 tháng 2', N'Q.1', N'Can Tho', N'NULL', N'NULL', N'NULL');
 

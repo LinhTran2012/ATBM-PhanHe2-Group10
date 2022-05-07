a8314aba902f29f54c8955686397596d3265467a
Nguyễn Thị Như Phương <92770267+nhuphuongs@users.noreply.github.com>
2022-05-07 15:43:27 +0700

    Update INSERT_DATA.sql

--- INSERT_DATA.sql
+++ INSERT_DATA.sql
@@ -1,6 +1,5 @@
 
-
---NHAP DU LIEU BANG CSYT
+--NHAP DU LIEU CSYT
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS1' ,'Benh vien da khoa Hong Duc','32/2 Thong Nhat - Phuong 10 - Quan Go Vap - TPHCM ', '0283996999');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS2' ,'Benh vien Cho Ray','201B Nguyen Chi Thanh - Phuong 12 - Quan 5 - TPHCM ', '0283996999');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS3' ,'Benh vien Rang Ham Mat Trung uong','263 Tran Hung Dao - Phuong Co Giang - Quan 1 - TPHCM ', '0283996999');
@@ -32,7 +31,7 @@ INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS28','Benh vien da 
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS29','Benh vien Quan 10','571 Su Van Hanh - Phuong 12 - Quan 10 - TPHCM ', '0283826978');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS30','Benh vien Quan 11','72 Duong so 5 - Phuong 8 - Quan 11 - TPHCM ', '0283996999');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS31','Benh vien Quan 12','111 Duong Thi Muoi - Phuong Tan Chanh Hiep - Quan 12 - TPHCM ', '0286507955');
-INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS32','Benh vien Quan Binh Tan','809 Huong lo 2 - Phuong Binh Trị Dong A - Quan Binh Tan - TPHCM ', '0283754955');
+INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS32','Benh vien Quan Binh Tan','809 Huong lo 2 - Phuong Binh Tr? Dong A - Quan Binh Tan - TPHCM ', '0283754955');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS33','Benh vien Quan Tan Binh','605 Hoang Van Thu - Phuong 4 - Quan Tan Binh - TPHCM ', '0283996999');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS34','Benh vien Quan Tan Phu','609 Au Co - Phuong Phu Trung - Quan Tan Phu - TPHCM ', '0238657948');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS35','Benh vien Quan Go Vap','641 Quang Trung - Phuong 11 - Quan Go Vap - TPHCM ', '0838945098');
@@ -52,9 +51,66 @@ INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS48','Benh vien Tai
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS49','Benh vien Tam thanh','766 Vo Van Kiet - Phuong 1 - Quan 5 - TPHCM ', '029234675');
 INSERT INTO CSYT(MACSYT, TENCSYT, DCCSYT, SDTCSYT) VALUES ('CS50','Benh vien Ung Buou','3 No Trang Long - Phuong 7 - Quan Binh Thanh - TPHCM ', '0283412637');
 
+--NHAP DU LIEU THANHTRA
+EXEC sp_THEMNHANVIEN('NV1',N'Do Khanh Ly',N'Nu','2001-12-12','371953100',N'Kien Giang','0868706271','CS1', N'Thanh tra',NULL);
+EXEC sp_THEMNHANVIEN('NV2',N'Do Tuong Vy',N'Nu','2000-12-12','371953101',N'Kien Giang','0777009958','CS2', N'Thanh tra',NULL);
+EXEC sp_THEMNHANVIEN('NV3',N'Kim Viet Tuy',N'Nam','1983-03-19','100015169',N'Tien Giang','0901121208','CS1', N'Thanh tra',NULL);
+EXEC sp_THEMNHANVIEN('NV4',N'Do Hong Yen',N'Nu','1994-04-02','100018356',N'Hung Yen','0231129748','CS5', N'Thanh tra',NULL);
+EXEC sp_THEMNHANVIEN('NV5',N'Muc Trong Vien',N'Nu','1997-09-21','100003843',N'Binh Phuoc','0831130931','CS7', N'Thanh tra',NULL);
 
-EXEC sp_THEMNHANVIEN('NV1',N'Do Khanh Ly', N'Nu', '2001-12-12','371953100', N'Kien Giang', '0868706271', 'CS1', N'Thanh tra', NULL);
-EXEC sp_THEMNHANVIEN('NV2',N'Do Tuong Vy', N'Nu', '2000-12-12','371953101', N'Kien Giang', '0777009958', 'CS1', N'Y si/bac si', 'CK1');
+--NHAP DU LIEU NGHIENCUU
+EXEC sp_THEMNHANVIEN('NV6',N'Khong Thanh Lac',N'Nam','1980-01-18','100015720',N'Nghe An','0781138425','CS43', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV7',N'Huynh Nguyen Dac',N'Nam','1990-04-28','100018157',N'Thanh Hoa','0371127802','CS43', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV8',N'Manh Nam Nhuong',N'Nam','1998-05-10','100022718',N'Quang Ngai','0981111240','CS47', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV9',N'Muc Kim Giap',N'Nam','1992-08-21','100022493',N'Ha Giang','0231119528','CS34', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV10',N'Truong Cao Nghiem',N'Nam','1988-03-04','100025297',N'Son La','0981141496','CS21', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV11',N'Tieu Khai Quy',N'Nam','1987-05-27','100012669',N'Dong Thap','0231114514','CS43', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV12',N'Ly Cuong Tiep',N'Nam','1987-10-15','100003727',N'Thanh pho Ha Noi','0831127506','CS28', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV13',N'Chung Ngoc Yen',N'Nu','1988-11-14','100032673',N'Bac Lieu','0371127436','CS5', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV14',N'Bui The Xuyen',N'Nu','1993-10-20','100017277',N'Dak Nong','0121111665','CS35', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV15',N'Lam Xuan Diep',N'Nu','1996-08-28','100023083',N'Ninh Binh','0971138235','CS36', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV16',N'Ly Manh Hy',N'Nu','1999-04-28','100024754',N'Binh Dinh','0981117714','CS37', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV17',N'Ngoc Duc Ky',N'Nam','1994-01-12','100015020',N'Vinh Long','0901121674','CS26', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV18',N'Luu Binh Luyen',N'Nu','1982-08-16','100024098',N'Lai Chau','0391121962','CS31', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV19',N'Ngiem Dai Thoa',N'Nu','1986-08-19','100010826',N'Thai Nguyen','0371137627','CS5', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV20',N'Hua an Yen',N'Nu','1988-06-03','100004126',N'Cao Bang','0981130806','CS35', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV21',N'Vu Tan Yen',N'Nu','1988-04-17','100003043',N'Dien Bien','0781134976','CS6', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV22',N'Doan Huy Tuan',N'Nam','1999-05-09','100004377',N'Hai Duong','0781123928','CS36', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV23',N'Trieu Nam Trac',N'Nam','2000-01-05','100009840',N'Thanh pho Da Nang','0371121386','CS5', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV24',N'Manh Hung Lieu',N'Nu','1995-02-06','100005549',N'Tien Giang','0971130400','CS23', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV25',N'La Quan Do',N'Nam','1980-06-05','100021420',N'Thanh pho Da Nang','0121122465','CS48', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV26',N'Ngoc Nghi Ngoc',N'Nu','1980-12-03','100011998',N'Ben Tre','0391131560','CS49', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV27',N'Doi Bich Phach',N'Nu','1993-04-17','100030454',N'Long An','0121123829','CS39', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV28',N'Cu Van Quang',N'Nam','1984-11-28','100016031',N'Hau Giang','0371123428','CS4', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV29',N'Luu Ha Ban',N'Nam','2000-12-16','100008308',N'Hai Duong','0971140468','CS35', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV30',N'Luu Phuong Yen',N'Nu','1995-06-18','100008851',N'Thai Nguyen','0901130809','CS36', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV31',N'Hua Bich Yen',N'Nu','1998-10-07','100000123',N'Quang Ninh','0781115420','CS22', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV32',N'Vo Vinh Khang',N'Nam','1996-12-11','100022807',N'Bac Ninh','0391112654','CS45', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV33',N'Tham Khanh Loi',N'Nam','1985-11-26','100003884',N'Bac Lieu','0281119827','CS41', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV34',N'Tong Chan Khoa',N'Nam','1986-06-03','100009342',N'Thanh Hoa','0281140640','CS6', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV35',N'Luu Tri Duong',N'Nam','1991-09-28','100003168',N'Vinh Phuc','0281130388','CS29', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV36',N'Lam Bich Yen',N'Nu','1985-10-21','100028828',N'Tuyen Quang','0981135193','CS29', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV37',N'Manh Nhan Ngung',N'Nu','1992-02-23','100015303',N'Hung Yen','0121118444','CS14', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV38',N'Hoang Hiep Toa',N'Nu','1996-03-11','100032390',N'Binh Phuoc','0391130723','CS16', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV39',N'Truong Ngoc Khiet',N'Nu','1997-04-16','100000333',N'Ninh Binh','0981119910','CS6', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV40',N'Dong Thien Doa',N'Nam','1981-06-22','100006809',N'Tuyen Quang','0281124921','CS21', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV41',N'Kieu Cuong Danh',N'Nam','1996-10-07','100016830',N'Son La','0231135556','CS33', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV42',N'Dao Thi Hanh',N'Nu','1998-08-01','100024762',N'Quang Ngai','0371123454','CS6', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV43',N'Muc Chi Yen',N'Nu','1992-06-27','100011179',N'Khanh Hoa','0371120686','CS46', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV44',N'Doi Tuan Dam',N'Nam','1987-08-23','100021214',N'Hung Yen','0901123026','CS48', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV45',N'Doan Chi Xuyen',N'Nam','1990-01-24','100019597',N'Tien Giang','0371143087','CS9', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV46',N'Hoang Huu Au',N'Nam','1985-03-19','100017866',N'Soc Trang','0981119841','CS14', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV47',N'Luc Cao Ham',N'Nam','1982-11-17','100005673',N'Dien Bien','0231116318','CS30', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV48',N'Banh Hoai Nguyet',N'Nu','2000-09-03','100026653',N'Vinh Phuc','0391115838','CS12', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV49',N'Cu Nam Tuan',N'Nam','1997-08-23','100014388',N'Gia Lai','0231137497','CS5', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV50',N'Moc Hoang Loi',N'Nam','1980-03-13','100026508',N'Nghe An','0231111408','CS12', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV51',N'Banh Tri Chanh',N'Nam','1981-06-09','100029240',N'Lam Dong','0391115768','CS36', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV52',N'Vu Tan Boc',N'Nam','1992-02-25','100030830',N'Tien Giang','0901122507','CS23', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV53',N'Dinh Bach Quyen',N'Nu','1989-09-19','100016822',N'Lang Son','0831113792','CS27', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV54',N'Muc Hong Son',N'Nam','1986-10-25','100017273',N'Binh Phuoc','0831112578','CS8', N'Nghien cuu', NULL);
+EXEC sp_THEMNHANVIEN('NV55',N'Duong Long Chau',N'Nu','1998-04-01','100004238',N'Ca Mau','0121121239','CS25', N'Nghien cuu', NULL);
+
+
 
 EXEC sp_THEMBENHNHAN('BN1','CS1', N'Tran Van Anh','916773394', '1954-03-11', 148, N'3 tháng 2', N'Q.1', N'Can Tho', N'NULL', N'NULL', N'NULL');
 

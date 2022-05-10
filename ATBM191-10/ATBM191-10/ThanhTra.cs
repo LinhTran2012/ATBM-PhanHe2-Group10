b19b11801f17cb7ed60dd053034cbd40c33f9dd4
Tracey Nguyen <92735451+nhhtam@users.noreply.github.com>
2022-05-10 17:24:35 +0700

    Update ThanhTra.cs

--- ATBM191-10/ATBM191-10/ThanhTra.cs
+++ ATBM191-10/ATBM191-10/ThanhTra.cs
@@ -20,6 +20,89 @@ namespace ATBM191_10
         {
             InitializeComponent();
             this.con = connect;
+            this.HienThiThongTin();
+        }
+        private void HienThiThongTin()
+        {
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM QLYCSYT.V_NHANVIEN_THONGTINCANHAN";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dr.Close();
+
+            lb_name.Text = dt.Rows[0][1].ToString();
+
+            dgv_info.DataSource = dt;
+        }
+
+        private void HienThiDuLieu()
+        {
+            OracleCommand cmd = con.CreateCommand();
+            if (cb_chon.Text == "Nhân Viên")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.NHANVIEN";
+            else if (cb_chon.Text == "Bệnh Nhân")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.BENHNHAN";
+            else if (cb_chon.Text == "HSBA")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.HSBA";
+            else if (cb_chon.Text == "HSBA_DV")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.HSBA_DV";
+            else
+                cmd.CommandText = "SELECT * FROM QLYCSYT.CSYT";
+
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dr.Close();
+
+            dgv_doc.DataSource = dt;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+                this.HienThiThongTin();
+            else if (tabControl1.SelectedIndex == 1)
+                this.HienThiDuLieu();
+                
+        }
+
+        private void btn_dangxuat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            f_DangNhap dn = new f_DangNhap();
+            dn.Show();
+        }
+
+        private void btn_chon_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = con.CreateCommand();
+            if (cb_chon.Text == "Nhân Viên")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.NHANVIEN";
+            else if (cb_chon.Text == "Bệnh Nhân")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.BENHNHAN";
+            else if (cb_chon.Text == "HSBA")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.HSBA";
+            else if (cb_chon.Text == "HSBA_DV")
+                cmd.CommandText = "SELECT * FROM QLYCSYT.HSBA_DV";
+            else
+                cmd.CommandText = "SELECT * FROM QLYCSYT.CSYT";
+
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                cmd.CommandType = CommandType.Text;
+                OracleDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dr.Close();
+
+                dgv_doc.DataSource = dt;
+            }
+            catch (Exception exp) { }
         }
     }
 }
+

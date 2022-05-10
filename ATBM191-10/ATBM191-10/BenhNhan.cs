96ff0cf64b84ef97d5dd878052708ca39aa42d5b
Tracey Nguyen <92735451+nhhtam@users.noreply.github.com>
2022-05-10 17:34:17 +0700

    Update BenhNhan.cs

--- ATBM191-10/ATBM191-10/BenhNhan.cs
+++ ATBM191-10/ATBM191-10/BenhNhan.cs
@@ -21,6 +21,52 @@ namespace ATBM191_10
         {
             InitializeComponent();
             this.con = connect;
+            this.HienThiThongTin();
         }
-    }
+
+        private void HienThiThongTin()
+        {
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM QLYCSYT.V_BENHNHAN_THONGTINCANHAN";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dr.Close();
+
+            lb_name.Text = dt.Rows[0][1].ToString();
+
+            dgv_info.DataSource = dt;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+                this.HienThiThongTin();
+        }
+        private void btn_dangxuat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            f_DangNhap dn = new f_DangNhap();
+            dn.Show();
+        }
+
+        private void btn_chinhsua_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "UPDATE QLYCSYT.V_BENHNHAN_THONGTINCANHAN " +
+                               "SET HOTEN='" + txt_hoten.Text + "' , NGAYSINH=TO_DATE('" + txt_ngaysinh.Text + "','dd/mm/yyyy')" +
+                               " , CMND ='" + txt_CMND.Text + "' ,SODT='" + txt_SDT.Text;
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i == 1)
+                {
+                    MessageBox.Show("Cập nhật thành công");
+                }
+            }
+            catch (Exception exp) { MessageBox.Show(exp.Message); }
+        }
+    } 
 }
